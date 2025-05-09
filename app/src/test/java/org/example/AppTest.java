4bfbc0ea914a515aa45627592c3976de7431caa1
Srinesh Nisala <srineshnisala@gmail.com>
2025-05-09 05:38:03 +0000

    chore: code formatting

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertEquals(classUnderTest.getGreeting(), "Hello World!");
     }
